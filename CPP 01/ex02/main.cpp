da7bf4ef1d6ceacfcce44c0d65474de9a0304277
Dutschke julien <jdutschk@student.42mulhouse.fr>
2023-05-04 21:52:44 +0200

    Update main.cpp

--- CPP 01/ex02/main.cpp
+++ CPP 01/ex02/main.cpp
@@ -6,15 +6,16 @@ int main()
 
 
     std::string str = "HI THIS IS BRAIN";
-    std::string* stringPTR = &str;
-    std::string& stringREF = str;
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+    
 
-    td::cout << "Address of the string in memory: " << &str << std::endl;
+    std::cout << "Address stored for str     : " << &str << std::endl;
     std::cout << "Address stored in stringPTR: " << stringPTR << std::endl;
     std::cout << "Address stored in stringREF: " << &stringREF << std::endl;
 
-
-    std::cout << "Valeur de la string : " << str << std::endl;
+    std::cout << "\n\n";
+    std::cout << "Valeur de la string          : " << str << std::endl;
     std::cout << "Valeur pointée par stringPTR : " << *stringPTR << std::endl;
     std::cout << "Valeur pointée par stringREF : " << stringREF << std::endl;
 }
